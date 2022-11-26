d775557a9b3bb775d0788c7864c46c686a64ac56
uni20181257 <chan4184@gmail.com>
2022-11-26 23:31:17 +0900

    Chanhyeok

--- DataStructure/HW4/3/3.cpp
+++ DataStructure/HW4/3/3.cpp
@@ -7,40 +7,106 @@ patterns.
 */
 
 #include <iostream>
-#include <map>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-void lastOccurenceFunction(string P, map<char, int>* alphabet) {
-    for(map<char, int>::iterator iter = alphabet->begin(); iter != alphabet->end(); iter++) {
-        char c = iter->first;
-        int max_idx = -1;
-        for(int i = 0; i < P.length(); i++) {
-            if(P[i] == c) {
-                max_idx = max(max_idx, i);
-            }
+int BruteForceMatch(string T, string P) {
+    int n = T.length();
+    int m = P.length();
+    for(int i = 0; i <= n-m; i++) {
+        // test shift i of the pattern
+        int j = 0; // index of pattern
+        while(j < m && T[i+j] == P[j])
+            j++;
+        if(j == m)
+            return i; // match at i
+        else // j < m
+            continue;
+            // break; // mismatch
+    }
+    return -1;
+}
+
+void failureFunction(string P, int* F) {
+    int m = P.length();
+    int i = 1;
+    int j = 0;
+    while(i < m) {
+        if(P[i] == P[j]) {
+            // we have matched j+1 chars
+            F[i] = j + 1;
+            i++;
+            j++;
+        } else if(j > 0) {
+            // use failure function to shift P
+            j = F[j - 1];
         }
-        iter->second = max_idx;
+        else {
+            F[i] = 0; // no match
+            i++;
+        }
     }
 }
 
-int BoyerMooreMatch(string T, string P, map<char, int>* alphabet) {
+int KMPMatch(string T, string P) {
+    int n = T.length();
+    int m = P.length();
+    int F[100] = { 0, }; failureFunction(P, F); // temporarily array size is assigned to 100 
+    // initialize starting index for comparison
+    int i = 0;
+    int j = 0;
+    while(i < n) {
+        if(T[i] == P[j]) {
+            if(j == m-1)
+                return i - j; // match
+            else {
+                i++;
+                j++;
+            }
+        } else {
+            if(j > 0)
+                j = F[j - 1];
+            else
+                i++;
+        }
+    }
+    return -1; // no match
+}
+
+
+void lastOccurenceFunction(string P, int* alphabet) {
+    // A ~ Z, a ~ z -> ASCII code: 65 ~ 90, 97 ~ 122
+    int m = P.length();
+    for(int i = 0; i < 256; i++)
+        alphabet[i] = -1; // all elements are initalized with -1
+    for(int i = 0; i < m; i++) {
+        alphabet[(int)P[i]] = i; // if duplicate characters are in P, a last character index will be set to alphabet array
+    }
+}
+
+
+int BoyerMooreMatch(string T, string P) {
+    int alphabet[256] = { 0, };
     lastOccurenceFunction(P, alphabet);
     int n = T.length();
     int m = P.length();
+    // initialize starting index for comparison
     int i = m - 1;
     int j = m - 1;
-    while(i > n-1) {
+    while(i <= n-1) {
         if(T[i] == P[j]) {
-            if(j == 0) return i; // match at i
+            if(j == 0) 
+                return i; // match at i
             else {
                 i--;
                 j--;
             }
         } else {
             // character-jump
-            int l = alphabet->find(T[i])->second;
+            int l = alphabet[(int)T[i]];
             i += m - min(j, 1 + l);
             j = m - 1;
         }
@@ -49,22 +115,45 @@ int BoyerMooreMatch(string T, string P, map<char, int>* alphabet) {
 }
 
 int main() {
-    map<char, int> alphabet;
-    // a ~ z ASCII code: 97 ~ 128
-    for(int i = 0; i < 26; i++) {
-        char c = (char)(i + 97);
-        alphabet.insert({c, 0});
-    }
-
     // Test #1
-    string T = "abacaabadcabacabaabb";
-    string P = "abacab";
-    // Output: 
+    // string T = "abacaabadcabacabaabb";
+    // string P = "abacab";
+    // Output: 10
 
-    // cin >> T;
-    // cin >> P;
+    string T = ""; string P = "";
+    cout << "Input your text: "; cin >> T;
+    cout << "Input your pattern for comparison with your text: "; cin >> P;
 
-    cout << "Where is matched? " << BoyerMooreMatch(T, P, &alphabet) << '\n';
+    // Brute-Force Pattern Matching: O(nm)
+    // Example of worst case: T = aaa ... ah, P = aaah
+    clock_t start_BruteForce = clock();
+    cout << "Where is matched? " << BruteForceMatch(T, P) << '\n'; // Output: 10
+    clock_t end_BruteForce = clock();
+    double time_BruteForce = double(end_BruteForce - start_BruteForce);
+    
+    // KMP Failure function: O(m)
+    // Knuth-Morris-Pratt's algorithm: O(m+n) is faster than the brute-force algorithm
+    clock_t start_KMP = clock();
+    cout << "Where is matched? " << KMPMatch(T, P) << '\n';
+    clock_t end_KMP = clock();
+    double time_KMP = double(end_KMP - start_KMP);
+
+    // last-occurrence function can be computed in time O(m+s)
+    // Brute-Force Pattern Matching: O(nm + s) is significiantly faster than the brute-force algorithm
+    //  1. Looking-glass heuristic
+    //  2. Character-jump hueristic
+    // Example of worst case: T = aaa ... a, P = baaa
+    clock_t start_BM = clock();
+    cout << "Where is matched? " << BoyerMooreMatch(T, P) << '\n';
+    clock_t end_BM = clock();
+    double time_BM = double(end_BM - start_BM);
+
+    cout << "Time for BruteForceMatch: " << time_BruteForce << " ms \n";
+    cout << "Time for KMP: " << time_KMP << " ms \n";
+    cout << "Time for BM: " << time_BM << " ms \n";
+
+    // Performance of KMP and BM will be different according to a given text and pattern
+    // In my experiment, results show that the BM algorithm is a little bit faster than the KMP algorithm
 
     return 0;
 }
--- PrincipleProgrammingLanguage/hw4/src/main/scala/Hw4.scala
+++ PrincipleProgrammingLanguage/hw4/src/main/scala/Hw4.scala
@@ -232,7 +232,6 @@ object MiniCInterpreter {
     case BeginEnd(expr) => eval(env, mem, expr)
     case FieldAccess(record, field) => {
       val E = eval(env, mem, record)
-<<<<<<< HEAD
       val E_prime = E.v match {
         case RecordVal(x, loc, next) => {
           if (x == field) E.m.get(loc) match {
@@ -247,14 +246,10 @@ object MiniCInterpreter {
         case _ => throw new UndefinedSemantics(s"message ${expr}")
       }
       E_prime
-=======
-      Result(E.m.get(E.v(field)), E.m)
->>>>>>> ae3190d565c81c95dee28bb2de9782bc00cd86e3
     }
     case FieldAssign(record, field, new_val) => {
       val E1 = eval(env, mem, record)
       val E2 = eval(env, E1.m, new_val)
-<<<<<<< HEAD
       val loc = E1.v match {
         case RecordVal(x, loc, next) => {
           if (x == field) loc
@@ -278,28 +273,12 @@ object MiniCInterpreter {
         case ProcVal(args, expr_0, env_prime) => {
           val (cal_env, cal_mem) = UpdatePCALLV(arg, args, env, env_prime, mem)
           eval(cal_env, cal_mem, expr_0)
-=======
-      val new_mem = E2.m + (E1.v(field) -> E2.v)
-      Result(E2.v, new_mem)
-    }
-    case Block(f, s) => {
-      
-    }
-    case PCallV(ftn, arg) => {
-      // how to process a list of arg??
-      val E0 = eval(env, mem, ftn)
-      val E = E0.v match {
-        case ProcVal(args, expr, env_prime) => {
-          val new_env = env_prime + (args -> env(arg))
-          eval(new_env, E0.m, expr)
->>>>>>> ae3190d565c81c95dee28bb2de9782bc00cd86e3
         }
         case _ => throw new UndefinedSemantics(s"message ${expr}")
       }
       E
     }
     case PCallR(ftn, arg) => {
-<<<<<<< HEAD
       // how to process a list of arg??
       val E0 = eval(env, mem, ftn)
       val E = E0.v match {
@@ -310,15 +289,6 @@ object MiniCInterpreter {
         case _ => throw new UndefinedSemantics(s"message ${expr}")
       }
       E
-=======
-      val E0 = eval(env, mem, ftn)
-      val E = E0.v match {
-        case ProcVal(args, expr, env_prime) => {
-
-        }
-        case _ => throw new UndefinedSemantics(s"message ${expr}")
-      }
->>>>>>> ae3190d565c81c95dee28bb2de9782bc00cd86e3
     }
     case WhileExpr(cond, body) => {
       val E1 = eval(env, mem, cond)
@@ -333,18 +303,19 @@ object MiniCInterpreter {
     }
     case EmptyRecordExpr => Result(EmptyRecordVal, mem)
     case RecordExpr(field, initVal, next) => {
-<<<<<<< HEAD
-      val E = eval(env, mem, initVal)
-      val (new_mem, loc) = E.m.extended(E.v)
-      val new_env = env + (field -> loc)
-      val E_prime = eval(env, new_mem, next).v match {
-        case RecordVal(field, loc, next_val) => RecordVal(field, loc, next_val)
-        case _ => throw new UndefinedSemantics(s"message ${expr}")
+      eval(env, mem, initVal) match {
+        case Result(v, m) => {
+          val (new_mem, new_loc) = m.extended(v)
+          val E = eval(env, new_mem, next) match {
+            case Result(next_v, next_m) => next_v match {
+              case RecordVal(next_field, next_loc, next_val) => RecordVal(next_field, next_loc, next_val)
+              case EmptyRecordVal => EmptyRecordVal
+              case _ => throw new UndefinedSemantics(s"message ${expr}")
+            }
+          }
+          Result(RecordVal(field, new_loc, E), new_mem)
+        }
       }
-      Result(E_prime, new_mem)
-=======
-      
->>>>>>> ae3190d565c81c95dee28bb2de9782bc00cd86e3
     }
     case _ => throw new UndefinedSemantics(s"message ${expr}")
   }
